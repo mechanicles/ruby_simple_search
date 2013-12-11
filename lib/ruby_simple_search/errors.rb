cb8cd35e41a3bca1fdaffe5a9239eb1eb9b459de
Santosh Wadghule <santosh.wadghule@gmail.com>
2013-12-11 23:33:32 +0530

    Remove unwanted line

--- lib/ruby_simple_search/errors.rb
+++ lib/ruby_simple_search/errors.rb
@@ -6,6 +6,5 @@ module RubySimpleSearch
     INVALID_TYPE           = "Extended query is not an array type"
     INVALID_CONDITION      = "Extended query's array conditions are wrong"
     INVALID_PATTERN        = "Pattern is wrong. it should be in the list #{RubySimpleSearch::LIKE_PATTERNS.keys}"
-    INVALID_CASE_SENSITIVE = "Case sensitive value is invalid"
   end
 end
